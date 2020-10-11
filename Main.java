08e4569e7956851ace3fa21675e19b9a6346a493
Nektarinee <71876309+Nektarinee@users.noreply.github.com>
2020-10-11 23:51:51 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package ru.GeekBrains;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+       Employee eployee1 = new Employee();
+        Employee[] persArray = new Employee[5];
+        persArray[0] = new Employee("Ivanov Ivan0", "Engineer0", "ivivan@mailbox0.com", "892312310", 30000, 30);
+        persArray[1] = new Employee("Ivanov Ivan1", "Engineer1", "ivivan@mailbox1.com", "89231231", 30001, 31);
+
+
+    }
+
+}
